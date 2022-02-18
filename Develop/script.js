b6341d9be8d5ecc38a19d6fd3cf694f68866d074
Dan N <dn691997@gmail.com>
2022-02-18 10:17:59 -0800

    update code for js

--- Develop/script.js
+++ Develop/script.js
@@ -1,61 +1,203 @@
 // Assignment code here
-function arrayFromLowToHigh(low, high){
+// var resultEl = document.getElementById('password')
+// var characterLengthEl = num
+// var lowerCaseEl = lowerCase
+// var upperCaseEl = upperCase
+// var numberEl = numeric
+// var specialCharEl = specialChar
+// var generateEl = generateBtn
 
-  var array = []
-  for (let i = low; i <= high; i++){
-    array.push(i)
-  }
-  return array;
-}
+// var randomFunc = {
+//   lower: getRandomLower,
+//   upper: getRandomUpper,
+//   number: getRandomNumber,
+//   Symbol: getRandomSymbol
+// };
 
-var UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65,90)
-var LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
-var NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
-var SPECIAL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126))
+// function getRandomLower () {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
+// console.log(getRandomLower());
 
-function generatePassword(characteramount, lowercase, uppercase, numeric, specialcharacter) {
+// function getRandomUpper () {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// }
+// console.log(getRandomUpper());
 
-  var characterAmount = (1,128);
-  String.fromCharCode(65);
-  console.log(LOWERCASE_CHAR_CODES);
+// function getRandomNumber () {
+//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+// }
+// console.log(getRandomNumber());
 
-  var pickNumber = window.prompt("How many characters? Must be at least 8, Maximum of 128");
+// function getRandomSymbol() {
+//   return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+// }
+// console.log(getRandomSymbol());
+// function arrayFromLowToHigh(low, high){
+
+//   var array = []
+//   for (let i = low; i <= high; i++){
+//     array.push(i)
+//   }
+//   return array;
+// }
+
+// const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65,90)
+// const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+// const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+// const SPECIAL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126))
+
+var specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+function generatePassword() {
+  var pickNumber = window.prompt(
+    "How many characters? Must be at least 8, Maximum of 128"
+  );
   console.log(typeof pickNumber);
   var num = parseInt(pickNumber);
   console.log(num);
 
-  if (pickNumber < 8 || pickNumber > 128 || pickNumber == "" || pickNumber == null) {
-    window.alert('Invalid response, please try again.');
+  if (
+    pickNumber < 8 ||
+    pickNumber > 128 ||
+    pickNumber == "" ||
+    pickNumber == null
+  ) {
+    window.alert("Invalid response, please try again.");
     return generatePassword();
   }
-  
   console.log(pickNumber);
-  var lowerCase= window.confirm("Do you want to include lowercase?");
-  console.log(lowerCase);
-  var upperCase=window.confirm("Do you want to include uppercase?");
-  console.log(upperCase);
-  var numeric=window.confirm(' Do you want to include number?');
-  console.log(numeric);
-  var specialChar=window.confirm('Do you want to include special character?');
-  console.log(specialChar);
-debugger;
-  var charCodes = LOWERCASE_CHAR_CODES
-  if(upperCase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-  if(numeric) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
-  if(specialcharacter) charCodes = charCodes.concat(SPECIAL_CHAR_CODES)
-  var passwordCharacters = []
-  for (let i = 0; i < characterAmount, i++) {
-    var characterCode = charCodes[Math.floor(Math.random() * characterAmount)]
-    passwordCharacters.push(String.fromCharCode(charCodes))
-  }
-    return passwordCharacters.join('');
 
-  if (lowerCase == false && upperCase== false && numeric == false && specialChar == false) {
-    window.confirm('Must pick AT LEAST one characteristic')
+  var lowerCase = window.confirm("Do you want to include lowercase?");
+  console.log(lowerCase);
+
+  var upperCase = window.confirm("Do you want to include uppercase?");
+  console.log(upperCase);
+
+  var numeric = window.confirm(" Do you want to include number?");
+  console.log(numeric);
+
+  var specialChar = window.confirm("Do you want to include special character?");
+  console.log(specialChar);
+
+  if (
+    lowerCase == false &&
+    upperCase == false &&
+    numeric == false &&
+    specialChar == false
+  ) {
+    window.confirm("Must pick AT LEAST one characteristic");
     return generatePassword();
- }
-  };
-  
+  }
+
+  var possibleCharCode = [];
+
+  if (lowerCase) {
+    possibleCharCode = possibleCharCode.concat(lowerCasedCharacters);
+  }
+  if (upperCase) {
+    possibleCharCode = possibleCharCode.concat(upperCasedCharacters);
+  }
+  if (numeric) possibleCharCode = possibleCharCode.concat(numericCharacters);
+  if (specialChar)
+    possibleCharCode = possibleCharCode.concat(specialCharacters);
+  console.log(possibleCharCode);
+  var password = "";
+  for (let i = 0; i < pickNumber; i++) {
+    var passwordChar =
+      possibleCharCode[Math.floor(Math.random() * possibleCharCode.length)];
+    password += passwordChar;
+  }
+  console.log(password);
+  return password;
+}
 
 //window.prompt "how many character 8 to 128 " link to the function generate password
 //if valid answer move to next prompt, if not then try again return to first prompt
@@ -70,11 +212,9 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
-   passwordText.value = password;
 
+  passwordText.value = password;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
